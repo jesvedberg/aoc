d21fd2764c473a47816e3d8934389cafb60b93c1
Jesper Svedberg <jsvedberg@gmail.com>
2022-12-08 23:03:28 +0100

    Added day3

---
+++ 2022/day03/version1.nim
@@ -0,0 +1,47 @@
+import sets, sequtils, strutils
+
+proc getDup(s: string): char =
+  let
+    lineMid = s.high div 2
+    p1 = s[0..lineMid].toHashSet
+    p2 = s[lineMid+1..^1].toHashSet
+  for item in p1 * p2:
+    return item
+
+proc getBadge(line, overlap: string): string =
+  let
+    s1 = line.toHashSet
+    s2 = overlap.toHashSet
+  return (s1 * s2).toSeq.join("")
+
+proc calcPrio(c: char): int =
+  let cnum = int(c)
+  if cnum > 90:
+    result = cnum - 96
+  else:
+    result = cnum - 64 + 26
+
+var
+  prioSum = 0
+  prioSum2 = 0
+  last = ""
+  counter = 0
+
+for line in "input".lines:
+  if line != "":
+    prioSum += line.getDup.calcPrio
+    if last.len > 0:
+      last = getBadge(line, last)
+      if counter == 2:
+        prioSum2 += last[0].calcPrio
+        last = ""
+        counter = 0
+      else:
+        inc counter
+    else:
+      last = line
+      inc counter
+
+echo prioSum
+echo prioSum2
+
